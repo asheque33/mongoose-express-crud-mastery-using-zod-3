9f32337effdd3dd77ac84d353a7263be465d8055
asheque33 <ashequrrahman3333@gmail.com>
2023-12-31 02:19:12 +0600

    User model added

--- src/app/modules/User/User.model.ts
+++ src/app/modules/User/User.model.ts
@@ -1,18 +1,25 @@
 /* eslint-disable @typescript-eslint/no-this-alias */
 import { Schema, model } from 'mongoose';
-import { IUser } from './user.interface';
+import { IUser } from './User.interface';
 import config from '../../config';
 import bcrypt from 'bcrypt';
 
-const userSchema = new Schema<IUser>(
-  {
-    username: { type: String, required: [true,'username is required'],unique: true },
-    email: { type: String, required: [true,'email is required'],unique: true },
-    password: { type: String, required: [true,'password is required'] },
-    
-    role: { type: String, enum: ['user', 'admin'],default: 'user', required: [true,'role is required'] },
-  }
-);
+const userSchema = new Schema<IUser>({
+  username: {
+    type: String,
+    required: [true, 'username is required'],
+    unique: true,
+  },
+  email: { type: String, required: [true, 'email is required'], unique: true },
+  password: { type: String, required: [true, 'password is required'] },
+
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+    required: [true, 'role is required'],
+  },
+});
 
 // userSchema.pre('save', async function (next) {
 //   const user = this;
